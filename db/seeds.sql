aa0107338a2a5d7a49033c7cd52aa6817dad2921
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-07 16:45:08 -0700

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('double patty');
+INSERT INTO burgers (burger_name) VALUES ('sans tomato');
+INSERT INTO burgers (burger_name, devoured) VALUES ('no lettuce', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('extra cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('single patty', true);
+INSERT INTO burgers (burger_name) VALUES ('animal style');
